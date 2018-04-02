5b9752526ead1111fdf094c394846b3bf5015fa3
FJ <1298616342@qq.com>
2018-04-03 01:02:28 +0800

    views

--- Represent/views.py
+++ Represent/views.py
@@ -3,22 +3,64 @@ from rest_framework.views import APIView
 from django.contrib.auth.decorators import login_required
 from Represent.models import Owner, Dog, Cat
 from rest_framework.response import Response
+from rest_framework import status, mixins
+from django.http import Http404
 from Represent.serializers import OwnerSerializer, DogSerializer, CatSerializer
-from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 from rest_framework import generics
 
 
 # Create your views here.
-class DogList(APIView):
-    def get(self, request, format=None):
-        dog = Dog.objects.all()
-        serializer = DogSerializer(dog, many=True)
-        return Response(serializer.data)
+class DogList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Dog.objects.all()
+    serializer_class = DogSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
     @login_required
-    def post(self, request, format=None):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class DogDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = DogSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    @login_required
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    @login_required
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+class CatList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    @login_required
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class CatDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    @login_required
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    @login_required
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
