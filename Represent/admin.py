f04e74f9207fd86947bc0d95e77e324cbc76d86d
FJ <1298616342@qq.com>
2018-04-08 00:00:26 +0800

    views

--- Represent/admin.py
+++ Represent/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Dog, Cat, Owner
+from .models import Dog, Cat
 
-Md = [Dog, Cat, Owner]
+Md = (Dog, Cat)
 for m in Md:
     admin.site.register(m)
